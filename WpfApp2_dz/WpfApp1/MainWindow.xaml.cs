5bd52a21256853339d1a5370c2e9c980a97dc2f9
21xxone <Timzar@bk.ru>
2022-01-08 00:23:39 +0300

    123

--- WpfApp2_dz/WpfApp1/MainWindow.xaml.cs
+++ WpfApp2_dz/WpfApp1/MainWindow.xaml.cs
@@ -28,10 +28,10 @@ namespace WpfApp1
         {
             InitializeComponent();
             timer.Interval = TimeSpan.FromSeconds(7);
-            timer.Tick += RandomButtons;
+            timer.Tick += RandomButtons1;
             timer.Start();
         }
-        private void RandomButtons(object sender, EventArgs e)
+        private void RandomButtons1(object sender, EventArgs e)
         {
             btns = new List<Button>() { btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
             List<int> number = new List<int>() { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
@@ -43,12 +43,12 @@ namespace WpfApp1
                 btn.Content = i;
                 btns.Remove(btn);
             }
-            timer.Interval = TimeSpan.FromSeconds(7);
         }
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             Button btn = sender as Button;
-            RandomButtons(btn, null);
+            RandomButtons1(btn, null);
+            text_box.Text += btn.Content;
         }
         private void btndelete_Click(object sender, RoutedEventArgs e)
         {
@@ -60,6 +60,35 @@ namespace WpfApp1
             }
         }
 
+        private void btnaccept_Click(object sender, RoutedEventArgs e)
+        {
+            if (text_box.Text.Length == 5)
+            {
+                text_box.Text = "Done";
+                btn0.IsEnabled = false;
+                btn1.IsEnabled = false;
+                btn2.IsEnabled = false;
+                btn3.IsEnabled = false;
+                btn4.IsEnabled = false;
+                btn5.IsEnabled = false;
+                btn6.IsEnabled = false;
+                btn7.IsEnabled = false;
+                btn8.IsEnabled = false;
+                btn9.IsEnabled = false;
+                btnaccept.IsEnabled = false;
+                btndelete.IsEnabled = false;
+                text_box.IsEnabled = false;
+            }
+            else
+            {
+                text_box.Text = "";
+            }
+        }
 
+        private void RandomButtons(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            RandomButtons1(btn, null);
+        }
     }
 }
